0dffdb34a5c17d8e86882065cb3aa85ce3caa901
yo_boi <abdelrahmanbbb@gmail.com>
2022-09-30 04:07:39 +0300

    added colon and double quote on left hand in both lower and upper layers

--- keyboards/lily58/keymaps/yo-boy/keymap.c
+++ keyboards/lily58/keymaps/yo-boy/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RCTRL |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LGUI |LOWER | LAlt | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   | LGUI |LAlt  |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |ctlalt|   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   ~  |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |LShift|      |      |      |      |      |-------|    |-------|   +  |   _  |   =  |   {  |   }  |   |  |
+ * |LShift|      |      |      |   "  |   :  |-------|    |-------|   +  |   _  |   =  |   {  |   }  |   |  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LGUI |LAlt  |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   A(KC_LCTL),KC_EXLM,KC_AT,  KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
-  KC_LSFT, _______, _______, _______, _______, _______, _______, _______, KC_PLUS, KC_UNDS, KC_EQL,  KC_LCBR, KC_RCBR, KC_PIPE,
+  KC_LSFT, _______, _______, _______, KC_DQT,  KC_COLN, _______, _______, KC_PLUS, KC_UNDS, KC_EQL,  KC_LCBR, KC_RCBR, KC_PIPE,
                              _______, _______, _______, _______, _______,  _______, _______, _______
 		  ),
 /* RAISE
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      | HOME | PGUP | PGDWN| END  |      |-------.    ,-------| PGUP | Left | Down |  Up  |Right |PGDWN |
  * |------+------+------+------+------+------|   [   |    |   ]   |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |RShift|
+ * |      |      |      |      |   "  |   :  |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |RCTRL |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LGUI | LAlt |LOWER | /Space  /       \Enter \  |RAISE |Delete| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______,                     KC_PSCR, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU,
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
   XXXXXXX, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  XXXXXXX,                     KC_PGUP, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGDN,
-  _______, _______, _______, _______, _______, _______,  _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
+  _______, _______, _______, _______, KC_DQT,  KC_COLN,  _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
                              _______, _______, _______,  _______, _______,  _______, KC_DEL,  _______
 		  )
 };
