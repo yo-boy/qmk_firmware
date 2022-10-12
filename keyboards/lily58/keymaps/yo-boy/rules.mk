65d082aab45dab52b95381432cf95d56920f2b8d
yo_boi <abdelrahmanbbb@gmail.com>
2022-10-12 23:52:38 +0300

    commented uneccessary code.

--- keyboards/lily58/keymaps/yo-boy/rules.mk
+++ keyboards/lily58/keymaps/yo-boy/rules.mk
@@ -11,10 +11,10 @@ SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_ENABLE= no             # OLED display
 
 # If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/rgb_state_reader.c \
-        ./lib/layer_state_reader.c \
-        ./lib/logo_reader.c \
-        ./lib/keylogger.c \
+#SRC +=  #./lib/rgb_state_reader.c \
+        #./lib/layer_state_reader.c \
+        #./lib/logo_reader.c \
+        #./lib/keylogger.c \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
         # ./lib/timelogger.c \
